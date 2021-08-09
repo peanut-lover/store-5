7ad1676face78dc5ecd2a96b3017e51b7012b890
Kwongiseok <ri2377@naver.com>
2021-08-09 17:22:05 +0900

    setting : webpack 설정

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <>hi</>;
+
+export default App;
---
+++ client/webpack.dev.js
@@ -0,0 +1,17 @@
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.config.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    publicPath: "/",
+    overlay: true,
+    port: 8081,
+    stats: "errors-only",
+    historyApiFallback: true,
+    proxy: {},
+  },
+});
