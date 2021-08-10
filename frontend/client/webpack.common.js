5c2e371acb703a4c7b4abccb067333bb6cde0629
peanut-lover <jihoson94@gmail.com>
2021-08-10 20:33:48 +0900

    add: client 절대 경로 설정

--- frontend/client/webpack.common.js
+++ frontend/client/webpack.common.js
@@ -1,34 +1,38 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+console.log(path.resolve(__dirname, 'src'));
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
 
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
   },
 
   module: {
     rules: [
       {
         test: /\.ts(x)?$/,
-        use: ["babel-loader", "ts-loader"],
+        use: ['babel-loader', 'ts-loader'],
       },
     ],
   },
 
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
   },
 
   plugins: [
     new webpack.ProvidePlugin({
-      React: "react",
+      React: 'react',
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
