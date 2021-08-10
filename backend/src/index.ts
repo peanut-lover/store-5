9a8fe602c92ff70df48b40800fd93c0c04a6ce55
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-10 19:40:47 +0900

    setting: src 디렉토리 추가

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-
-createConnection()
-  .then(async (connection) => {
-    console.log('데이터베이스 연결');
-  })
-  .catch((error) => console.log(error));
