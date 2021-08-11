0e09a79470d6facd9f667244c5a770ff40a9ee9c
eojin-dev <eojin-dev@naver.com>
2021-08-11 12:39:32 +0900

    update : CheckButton 아이콘

--- frontend/client/src/components/CheckButton/CheckButton.tsx
+++ frontend/client/src/components/CheckButton/CheckButton.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaCheck } from 'react-icons/fa';
 
 const StyledButton = styled.button`
   cursor: pointer;
-  width: 1.5rem;
-  height: 1.5rem;
   border-radius: 0.125rem;
   transition: 0.2s linear;
+  width: 1.5rem;
+  height: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   background-color: ${(props) => (props.value ? 'rgb(42, 193, 188)' : 'white')};
   border: 1px solid ${(props) => (props.value ? 'rgb(42, 193, 188)' : '#ddd')};
@@ -18,7 +22,11 @@ interface Props {
 }
 
 const CheckButton: React.FC<Props> = ({ value, onClick }) => {
-  return <StyledButton value={value ? 'true' : ''} onClick={onClick}></StyledButton>;
+  return (
+    <StyledButton value={value ? 'true' : ''} onClick={onClick}>
+      <FaCheck color='white' />
+    </StyledButton>
+  );
 };
 
 export default CheckButton;
