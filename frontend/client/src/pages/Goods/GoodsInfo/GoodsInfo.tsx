85e54dfacda90c0a20b68e81b5852378fb3f802a
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-11 23:18:47 +0900

    add: GoodsInfo 컴포넌트 스타일 작성

---
+++ frontend/client/src/pages/Goods/GoodsInfo/GoodsInfo.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { GoodsProps } from '@src/pages/Goods/Goods';
+import styled from 'styled-components';
+
+const calculateSalePrice = (price: number, discountRate: number) => price - price * (discountRate / 100);
+
+const GoodsInfo = ({ title, price, discountRate, deliveryFee, deliveryDetail, isWished }: GoodsProps) => {
+  const salePrice = calculateSalePrice(price, discountRate);
+
+  return (
+    <>
+      <GoodsTitle>{title}</GoodsTitle>
+      <GoodsContent>
+        {discountRate > 0 && (
+          <ContentItem>
+            <span>정가</span>
+            <Price>{price.toLocaleString()}원</Price>
+          </ContentItem>
+        )}
+        <ContentItem>
+          <span>판매가격</span>
+          <SalePrice>
+            <span>{salePrice.toLocaleString()}</span>원
+          </SalePrice>
+        </ContentItem>
+        <ContentItem>
+          <span>배송정보</span>
+          <DeliveryInfo>
+            <p>{deliveryFee.toLocaleString()}원</p>
+            <p>{deliveryDetail}</p>
+          </DeliveryInfo>
+        </ContentItem>
+      </GoodsContent>
+    </>
+  );
+};
+
+const GoodsTitle = styled.h2`
+  font-size: 1.875rem;
+  font-weight: 600;
+  margin-bottom: 3rem;
+  line-height: 1.5em;
+`;
+const GoodsContent = styled.div`
+  font-size: 1rem;
+`;
+const ContentItem = styled.div`
+  display: flex;
+  column-gap: 2rem;
+  align-items: flex-start;
+  margin-bottom: 1rem;
+  line-height: 1.5em;
+  span {
+    color: #717171;
+    width: 5rem;
+    font-weight: 600;
+  }
+`;
+const Price = styled.p`
+  text-decoration: line-through;
+`;
+const SalePrice = styled.p`
+  font-weight: 600;
+  span {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.3125rem;
+    color: #333;
+  }
+`;
+const DeliveryInfo = styled.div``;
+
+export default GoodsInfo;
