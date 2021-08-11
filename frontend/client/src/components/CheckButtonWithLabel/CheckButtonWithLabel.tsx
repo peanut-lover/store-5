996323f5b851b0f77c3a7870ff84b7af52e480e6
eojin-dev <eojin-dev@naver.com>
2021-08-11 11:46:05 +0900

    add : CheckButtonWithLabel

---
+++ frontend/client/src/components/CheckButtonWithLabel/CheckButtonWithLabel.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CheckButton from 'src/components/CheckButton/CheckButton';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  cursor: pointer;
+  padding-left: 0.5rem;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+interface Props {
+  value: boolean;
+  onClick: () => void;
+  label: string;
+}
+
+const CheckButtonWithLabel: React.FC<Props> = ({ value, onClick, label }) => {
+  return (
+    <Wrapper>
+      <CheckButton value={value} onClick={onClick}></CheckButton>
+      <Label onClick={onClick}>{label}</Label>
+    </Wrapper>
+  );
+};
+
+export default CheckButtonWithLabel;
