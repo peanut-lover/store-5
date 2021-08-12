c02925dff0ced71b48015be07cebb37ea962e5b2
peanut-lover <jihoson94@gmail.com>
2021-08-12 11:34:40 +0900

    add: Footer 구현

--- frontend/client/src/pages/Main/Main.tsx
+++ frontend/client/src/pages/Main/Main.tsx
@@ -4,6 +4,7 @@ import { ThumbnailGoods } from '@src/types/Goods';
 import GoodsSection from '@src/components/GoodsSection/GoodsSection';
 import PromotionCarousel from '@src/components/PromotionCarousel/PromotionCarousel';
 import { Promotion } from '@src/types/Promotion';
+import Footer from '@src/components/Footer/Footer';
 
 const mockProductImagePath =
   'https://user-images.githubusercontent.com/20085849/128866958-900ad32a-cd32-4b97-be79-1dbbc9dcb02d.jpeg';
@@ -75,6 +76,9 @@ const Main = () => (
       <GoodsSection sectionTitle='잘나가요' goodsList={mock_best_products} />
       <GoodsSection sectionTitle='새로 나왔어요' goodsList={mock_new_products} />
     </MainContentContainer>
+    <FooterContainer>
+      <Footer />
+    </FooterContainer>
   </>
 );
 
@@ -83,4 +87,13 @@ const MainContentContainer = styled.div`
   margin: 0 auto;
 `;
 
+interface FooterContainerProps {
+  theme: {
+    dustWhite: string;
+  };
+}
+const FooterContainer = styled.div<FooterContainerProps>`
+  width: 100%;
+  background-color: ${(props) => props.theme.dustWhite};
+`;
 export default Main;
