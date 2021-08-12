f55a7dd709aaa26261f5c3e11d58f01fc1001b6c
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-12 19:50:08 +0900

    update: 상세 페이지 인터랙티브 컴포넌트 props 정리 및 이벤트 추가

--- frontend/client/src/pages/GoodsDetail/GoodsInteractive/GoodsInteractive.tsx
+++ frontend/client/src/pages/GoodsDetail/GoodsInteractive/GoodsInteractive.tsx
@@ -1,24 +1,27 @@
 import GoodsButtons from './GoodsButtons/GoodsButtons';
 import GoodsAmount from './GoodsAmount/GoodsAmount';
 import { DetailGoods } from '@src/types/Goods';
-import React, { useCallback, useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 const GoodsInteractive = ({ id, title, price, deliveryFee, discountRate = 0, isWished = false }: DetailGoods) => {
   const [amount, setAmount] = useState(0);
-  useCallback(() => {}, []);
-  useCallback(() => {}, []);
-  useCallback(() => {}, []);
+
+  const handleToWish = useCallback(() => {
+    // TODO 찜하기 API 적용
+    // goodsId, userId 전달
+  }, []);
+  const addToCart = useCallback(() => {
+    // TODO 장바구니 API 적용
+    // goodsId, userId, amount 전달
+  }, []);
+  const addToOrder = useCallback(() => {
+    // TODO 결제 API 적용
+    // goodsId, userId, amount 전달
+  }, []);
   return (
     <div>
-      <GoodsAmount
-        title={title}
-        price={price}
-        deliveryFee={deliveryFee}
-        discountRate={discountRate}
-        amount={amount}
-        setAmount={setAmount}
-      />
-      <GoodsButtons isWished={isWished} goodsId={id} amount={amount} />
+      <GoodsAmount {...{ title, price, deliveryFee, discountRate, amount, setAmount }} />
+      <GoodsButtons {...{ isWished, handleToWish, addToCart, addToOrder }} />
     </div>
   );
 };
