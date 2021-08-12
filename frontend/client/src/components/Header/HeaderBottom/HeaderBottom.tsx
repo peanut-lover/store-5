2b7ac4adbc9b82b1f208b09a512f0df5b960e5ae
Kwongiseok <ri2377@naver.com>
2021-08-12 20:17:09 +0900

    add: 헤더 로고 컴포넌트화 수정

--- frontend/client/src/components/Header/HeaderBottom/HeaderBottom.tsx
+++ frontend/client/src/components/Header/HeaderBottom/HeaderBottom.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import CategoryToggle from './CategoryToggle/CategoryToggle';
+import HeaderLogo from './HeaderLogo/HeaderLogo';
 import HeaderIconContainer from './HeaderIconContainer/HeaderIconContainer';
-import logoIcon from '@src/../public/images/logo.png';
-import { Link } from '@src/lib/CustomRouter';
 
 const HeaderBottom = () => {
   return (
     <HeaderBottomContainer>
       <CategoryToggle />
-      <LogoContainer>
-        <Link to='/'>
-          <Logo src={logoIcon} />
-        </Link>
-      </LogoContainer>
+      <HeaderLogo />
       <HeaderIconContainer />
     </HeaderBottomContainer>
   );
 };
+
 const HeaderBottomContainer = styled.div`
   box-sizing: border-box;
   display: flex;
@@ -28,15 +24,6 @@ const HeaderBottomContainer = styled.div`
   padding: 0 15% 0 15%;
   border-bottom: 1px solid lightgray;
   border-top: 1px solid lightgray;
-`;
-const LogoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  height: 100%;
-  width: 156px;
-`;
-const Logo = styled.img`
-  width: 100%;
 `;
 
 export default HeaderBottom;
