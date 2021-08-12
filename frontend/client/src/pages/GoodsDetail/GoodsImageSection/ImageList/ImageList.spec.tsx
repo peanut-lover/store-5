fe243d0a5b4c23e69ef053413a9c5eaf8be6e7d5
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-12 18:06:19 +0900

    update: 상세 페이지 이미지 리스트 Props 변경
    - useState 관련 값 추가

---
+++ frontend/client/src/pages/GoodsDetail/GoodsImageSection/ImageList/ImageList.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+
+import ImageList from './ImageList';
+
+const testImageList: string[] = [
+  'https://user-images.githubusercontent.com/20085849/128860803-24b0e9a7-1482-4ca6-9a8c-ba30e7271e7a.jpeg',
+  'https://user-images.githubusercontent.com/20085849/128860803-24b0e9a7-1482-4ca6-9a8c-ba30e7271e7a.jpeg',
+  'https://user-images.githubusercontent.com/20085849/128860803-24b0e9a7-1482-4ca6-9a8c-ba30e7271e7a.jpeg',
+];
+
+const TestContainer = document.createElement('div');
+
+describe('ImageList Component', () => {
+  it('전달받은 이미지 배열의 길이만큼 imageItem을 생성해야 한다.', () => {
+    const wrapper = render(<ImageList imgs={testImageList} active={0} setActive={() => {}} />, {
+      container: document.body.appendChild(TestContainer),
+    });
+    expect(wrapper.container.children[0].childNodes).toHaveLength(testImageList.length);
+  });
+
+  afterAll(cleanup);
+});
