fcbf37f74766a7c8833b68bd09161aa80fe06098
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-15 03:03:39 +0900

    add: 상품 목록 카테고리 조회 조건 추가

--- backend/src/repository/goods.repository.ts
+++ backend/src/repository/goods.repository.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { GOODS_DB_ERROR } from '../constants/database-error-name';
 import { DatabaseError } from '../errors/base.error';
 import { Goods } from '../entity/Goods';
+import { CreateGoodsRequest } from '../types/request/goods.request';
 
 async function findGoodsDetailById({ id }: { id: number }) {
   try {
@@ -18,6 +19,49 @@ async function findGoodsDetailById({ id }: { id: number }) {
   }
 }
 
+async function createGoods(requestGoods: CreateGoodsRequest) {
+  const goodsRepo = getRepository(Goods);
+  const { title, price, stock, discountRate, state } = requestGoods;
+  const goods = await goodsRepo.create({
+    thumbnailUrl: 'as',
+    countOfSell: 123,
+    isGreen: false,
+    title: 'asd',
+    price: 300,
+    stock: 100,
+    discountRate: 10,
+    state: 'S',
+  });
+}
+
+async function findAllByCategory({
+  category,
+  offset,
+  limit,
+  order = 'createdAt',
+  sort = 'ASC',
+}: FindAllCategoryProps): Promise<Goods[] | undefined> {
+  try {
+    const goodsRepo = getRepository(Goods);
+
+    const data = await goodsRepo.find({
+      where: {
+        category,
+      },
+      skip: offset,
+      take: limit,
+      order: {
+        [order]: sort,
+      },
+    });
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new DatabaseError(GOODS_DB_ERROR);
+  }
+}
+
 export const GoodsRepository = {
   findGoodsDetailById,
+  findAllByCategory,
 };
