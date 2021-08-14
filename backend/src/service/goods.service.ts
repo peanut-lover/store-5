9cbdadc3c5150fd9206628a2760b75489c4bed80
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-15 03:10:45 +0900

    add: Goods Service 상품 목록 조회
    - getAllByCategory
    - getCategoryByFlag

--- backend/src/service/goods.service.ts
+++ backend/src/service/goods.service.ts
@@ -1,5 +1,30 @@
+import { Goods } from '../entity/Goods';
 import { GoodsRepository } from '../repository/goods.repository';
 import { DetailGoodsResponse } from '../types/response/goods.response';
+import { FindAllCategoryProps } from '../types/Goods';
+import { pagination } from '../utils/pagination';
+
+interface PageProps {
+  page: number;
+  limit: number;
+}
+
+type GetAllByCategoryProps = PageProps & {
+  // flag: 'best' | 'low' | 'high';
+  flag: string;
+  category: number;
+};
+
+type GetAllByKeywordProps = PageProps & {
+  keyword: string;
+};
+
+interface GoodsListMetaData {
+  page: number;
+  limit: number;
+  totalPage: number;
+  totalCount: number;
+}
 
 async function getDetailById(id: number): Promise<DetailGoodsResponse> {
   const data = await GoodsRepository.findGoodsDetailById({ id });
@@ -9,6 +34,23 @@ async function getDetailById(id: number): Promise<DetailGoodsResponse> {
   return { ...res, goodsImgs: imgs };
 }
 
+async function getAllByCategory({ category, page, flag = 'new', limit }: GetAllByCategoryProps) {
+  const option: FindAllCategoryProps = {
+    category,
+    offset: pagination.calculateOffset(page, limit),
+    limit,
+    order: getCategoryByFlag(flag) ?? 'createdAt',
+    sort: flag === 'low' ? 'DSC' : 'ASC',
+  };
+
+  await GoodsRepository.findAllByCategory(option);
+}
+
+function getCategoryByFlag(flag: string): keyof Goods {
+  return flag === 'low' || flag === 'high' ? 'price' : 'countOfSell';
+}
+
 export const GoodsService = {
   getDetailById,
+  getAllByCategory,
 };
