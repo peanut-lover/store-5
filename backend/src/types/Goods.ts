bc79b064f28960e9fb57aafb5cc4c2ad0876292a
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-15 11:25:43 +0900

    update: 상품 목록 조회 인자 인터페이스 수정
    - where 추가!

--- backend/src/types/Goods.ts
+++ backend/src/types/Goods.ts
@@ -3,6 +3,7 @@ import { Goods } from '../entity/Goods';
 export interface FindAllCategoryProps {
   category: number;
   offset: number;
+  where?: object;
   limit: number;
   order: keyof Goods;
   sort: 'ASC' | 'DSC';
