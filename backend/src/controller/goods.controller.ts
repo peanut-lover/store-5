e96456c96125f333704b2589ac3311af2d37c756
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-15 19:12:11 +0900

    update: 상품 목록 조회 query 유효성 검사
    - 간단하게 빈 값이나 undefined 문자열인지 확인하는 부분만 추가하였습니다.

--- backend/src/controller/goods.controller.ts
+++ backend/src/controller/goods.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { GoodsService } from '../service/goods.service';
+import { BadRequestError } from '../errors/client.error';
 import { GetAllGoodsRequest } from '../types/request/goods.request';
+import { INVALID_DATA } from '../constants/client-error-name';
 
 async function getGoodsDetail(req: Request, res: Response) {
   const goodsId = Number(req.params.id);
@@ -9,12 +11,23 @@ async function getGoodsDetail(req: Request, res: Response) {
 }
 
 async function getAllGoodsCategory(req: GetAllGoodsRequest, res: Response) {
+  const { category, page, flag, limit, state = 'S' } = req.query;
   const userId = req.userId;
+
+  if (checkValidationValue([category, page, flag, limit, state])) throw new BadRequestError(INVALID_DATA);
   const data = await GoodsService.getAllByCategoryInSalesState({
-    ...req.query,
+    category,
+    page,
+    flag,
+    limit,
+    state,
     userId,
   });
   return res.json(data);
+}
+
+function checkValidationValue(datas: string[]): boolean {
+  return Boolean(datas.find((data) => !data || data === 'undefined'));
 }
 
 export const GoodsController = {
