4345da553559589679ea660e89429310b11ad364
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-15 18:48:49 +0900

    add: 상품 목록 조회 라우터, 컨트롤러 추가

--- backend/src/controller/goods.controller.ts
+++ backend/src/controller/goods.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { GoodsService } from '../service/goods.service';
+import { GetAllGoodsRequest } from '../types/request/goods.request';
 
 async function getGoodsDetail(req: Request, res: Response) {
   const goodsId = Number(req.params.id);
@@ -7,6 +8,16 @@ async function getGoodsDetail(req: Request, res: Response) {
   res.status(200).json({ result });
 }
 
+async function getAllGoodsCategory(req: GetAllGoodsRequest, res: Response) {
+  const userId = req.userId;
+  const data = await GoodsService.getAllByCategoryInSalesState({
+    ...req.query,
+    userId,
+  });
+  return res.json(data);
+}
+
 export const GoodsController = {
   getGoodsDetail,
+  getAllGoodsCategory,
 };
