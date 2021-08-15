462e44631fa34c0d473806895d74453c652ce2c9
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-15 18:50:44 +0900

    update: 상품 관련 인터페이스 type으로 이동
    - 작업 중 변경될 수 있습니다!

--- backend/src/types/Goods.ts
+++ backend/src/types/Goods.ts
@@ -4,7 +4,33 @@ export interface FindAllCategoryProps {
   category: number;
   offset: number;
   where?: object;
+  userId?: number;
   limit: number;
   order: keyof Goods;
-  sort: 'ASC' | 'DSC';
+  sort: 'ASC' | 'DESC';
+}
+
+// interface PageProps {
+//   page: number;
+//   limit: number;
+//   userId?: number;
+// }
+
+// export type GetAllByCategoryProps = PageProps & {
+//   flag?: string;
+//   category?: number;
+//   state?: string;
+// };
+
+// export type GetAllByKeywordProps = PageProps & {
+//   keyword: string;
+// };
+
+export interface GetAllByCategoryProps {
+  category: string;
+  page: string;
+  flag: string;
+  limit: string;
+  state: string;
+  userId: number;
 }
