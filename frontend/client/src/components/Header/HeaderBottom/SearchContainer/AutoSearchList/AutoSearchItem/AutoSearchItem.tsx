2b9b56a88f7164b2305a935023f510811368206d
Kwongiseok <ri2377@naver.com>
2021-08-16 20:11:05 +0900

    add: AutoSearchItem 컴포넌트 추가

--- frontend/client/src/components/Header/HeaderBottom/SearchContainer/AutoSearchList/AutoSearchItem/AutoSearchItem.tsx
+++ frontend/client/src/components/Header/HeaderBottom/SearchContainer/AutoSearchList/AutoSearchItem/AutoSearchItem.tsx
@@ -11,18 +11,35 @@ const AutoSearchItem: React.FC<Props> = ({ onAddHistory, searchedItem }) => {
   const push = usePushHistory();
   const handleClick = async () => {
     await onAddHistory(searchedItem.title);
-    push(`/detail/${searchedItem.title}`);
+    push(`/detail/${searchedItem.id}`);
   };
-  return <AutoSearchItemContainer onClick={handleClick}></AutoSearchItemContainer>;
+  return (
+    <AutoSearchItemContainer onClick={handleClick}>
+      <AutoSearchItemImage src={searchedItem.thumbnailUrl} />
+      <AutoSearchItemTitle>{searchedItem.title}</AutoSearchItemTitle>
+    </AutoSearchItemContainer>
+  );
 };
 
 const AutoSearchItemContainer = styled.li`
+  position: relative;
+  display: flex;
+  align-items: center;
   margin-bottom: 12px;
-  line-height: 1.5em;
+  height: 30px;
   cursor: pointer;
   :hover {
     background-color: #e6e9e9;
   }
 `;
+
+const AutoSearchItemImage = styled.img`
+  width: 15%;
+  height: 100%;
+  margin-right: 15px;
+  border-radius: 8px;
+`;
+
+const AutoSearchItemTitle = styled.span``;
 
 export default AutoSearchItem;
