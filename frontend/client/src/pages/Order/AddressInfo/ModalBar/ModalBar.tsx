807c41510c2c3a7edf289215256deec9d8d4c7ed
eojin-dev <eojin-dev@naver.com>
2021-08-16 14:57:42 +0900

    add : ModalBar

---
+++ frontend/client/src/pages/Order/AddressInfo/ModalBar/ModalBar.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { FiX } from 'react-icons/fi';
+import styled from 'styled-components';
+
+interface Props {
+  title: string;
+  onClickExit?: () => void;
+}
+
+const ModalBar: React.FC<Props> = ({ title, onClickExit }) => {
+  return (
+    <Wrapper>
+      <FlexCenter>{title}</FlexCenter>
+      <FlexRight>
+        <IconButton onClick={onClickExit}>
+          <FiX size='1.5rem' />
+        </IconButton>
+      </FlexRight>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  font-weight: bolder;
+  padding: 0.75rem;
+  border-bottom: 1px solid #ddd;
+
+  ::before {
+    content: '';
+    flex: 1;
+  }
+`;
+
+const FlexCenter = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+const FlexRight = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const IconButton = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+`;
+
+export default ModalBar;
