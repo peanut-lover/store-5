48da57d6f25c3589e8b0b4e907ffd7629f33d5d8
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-16 17:59:37 +0900

    add: Wish 라우터 추가

---
+++ backend/src/router/wish.router.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import WishController from '../controller/wish.controller';
+import wrapAsync from '../utils/wrap-async';
+
+const router = express.Router();
+
+router.post('/goods', wrapAsync(WishController.createWish));
+router.delete('/goods/:wishId', wrapAsync(WishController.deleteWish));
+export default router;
