966b3560ae3b6f5586d2e0a5b38d989c75e718ba
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-16 17:57:16 +0900

    add: Wish repository 추가
    - createWish
    - deleteWish
    - findWishByIds

--- backend/src/repository/wish.repository.ts
+++ backend/src/repository/wish.repository.ts
@@ -1,7 +1,38 @@
-import { getRepository } from 'typeorm';
-import { WISH_DB_ERROR } from '../constants/database-error-name';
+import { DeleteResult, getRepository } from 'typeorm';
+import { USER_ADDRESS_DB_ERROR, WISH_DB_ERROR } from '../constants/database-error-name';
 import { Wish } from '../entity/Wish';
 import { DatabaseError } from '../errors/base.error';
+
+async function findWishByIds(userId: number, wishId: number): Promise<Wish | undefined> {
+  try {
+    const wishRepo = getRepository(Wish);
+    return await wishRepo.findOne({ where: { id: wishId, user: userId } });
+  } catch (err) {
+    console.error(err);
+    throw new DatabaseError(USER_ADDRESS_DB_ERROR);
+  }
+}
+
+async function createWish(userId: number, goodsId: number): Promise<Wish> {
+  try {
+    const wishRepo = getRepository(Wish);
+    const wish = await wishRepo.save({ userId, goodsId });
+    return wish;
+  } catch (err) {
+    console.error(err);
+    throw new DatabaseError(USER_ADDRESS_DB_ERROR);
+  }
+}
+
+async function deleteWish(wishId: number): Promise<DeleteResult> {
+  try {
+    const wishRepo = getRepository(Wish);
+    return await wishRepo.delete({ id: wishId });
+  } catch (err) {
+    console.error(err);
+    throw new DatabaseError(USER_ADDRESS_DB_ERROR);
+  }
+}
 
 async function findWishByUserId(userId: number): Promise<number[]> {
   try {
@@ -20,4 +51,7 @@ async function findWishByUserId(userId: number): Promise<number[]> {
 
 export const WishRepository = {
   findWishByUserId,
+  findWishByIds,
+  createWish,
+  deleteWish,
 };
