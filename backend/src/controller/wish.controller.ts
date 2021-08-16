1d025587109b109e1becc2a1b5ee76125bf206d1
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-16 18:41:39 +0900

    update: userId 매직넘버 제거 :)

--- backend/src/controller/wish.controller.ts
+++ backend/src/controller/wish.controller.ts
@@ -2,14 +2,14 @@ import { Request, Response } from 'express';
 import WishService from '../service/wish.service';
 
 async function createWish(req: Request, res: Response) {
-  const userId = 1;
+  const userId = req.userId;
   const goodsId = req.body.goodsId;
   const result = await WishService.createWish(userId, goodsId);
   res.status(201).json({ result });
 }
 
 async function deleteWish(req: Request, res: Response) {
-  const userId = 1;
+  const userId = req.userId;
   const goodsId = Number(req.params.goodsId);
   await WishService.deleteWish(userId, goodsId);
   res.sendStatus(204);
