910b7c8e20148124a0150fedb6b7461d35a74f39
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-17 10:24:36 +0900

    update: 카테고리 이름 decodeURI 처리

--- frontend/client/src/pages/CategoryGoods/CategoryGoodsList/CategoryGoodsList.tsx
+++ frontend/client/src/pages/CategoryGoods/CategoryGoodsList/CategoryGoodsList.tsx
@@ -1,6 +1,5 @@
-import { getGoodsByCategory } from '@src/apis/goodsAPI';
+import { getGoodsByCategory, GetGoodsByCategoryProps } from '@src/apis/goodsAPI';
 import GoodsSection from '@src/components/GoodsSection/GoodsSection';
-import { Link } from '@src/lib/CustomRouter';
 import Paginator from '@src/pages/CategoryGoods/CategoryGoodsList/Paginator';
 import { ThumbnailGoods } from '@src/types/Goods';
 import React, { useEffect, useState } from 'react';
@@ -10,30 +9,66 @@ interface Props {
   category: string;
 }
 
-const CategoryGoodsList: React.FC<Props> = ({ category }) => {
-  const [goodsList, setGoodsList] = useState<ThumbnailGoods[]>([]);
+interface GoodsListMap {
+  goodsList: ThumbnailGoods[];
+  meta: {
+    page: number;
+    limit: number;
+    totalPage: number;
+    totalCount: number;
+  };
+}
 
-  const fetchGoodsList = async (categoryName: string) => {
-    const data = await getGoodsByCategory(categoryName);
-    setGoodsList(data.result.goods);
+const GoodsFlag = {
+  best: 'best',
+  low: 'low',
+  high: 'high',
+  latest: 'latest',
+};
+
+const CategoryGoodsList: React.FC<Props> = ({ category }) => {
+  const [goodsListMap, setGoodsListMap] = useState<GoodsListMap | null>(null);
+  const [searchQuery, setSearchQuery] = useState<GetGoodsByCategoryProps>({
+    categoryName: category,
+    page: 1,
+    flag: GoodsFlag.latest,
+  });
+
+  const fetchGoodsList = async () => {
+    const data = await getGoodsByCategory(searchQuery);
+    setGoodsListMap(data.result);
+  };
+
+  // TODO: 플래그 변경시 page는 1? 아니면 유지?
+  const onChangeFlag = (flag: string) => {
+    setSearchQuery({
+      ...searchQuery,
+      page: 1,
+      flag,
+    });
   };
 
   useEffect(() => {
-    console.log('url 변경');
-    fetchGoodsList(category);
-  }, [location.search]);
+    fetchGoodsList();
+  }, [searchQuery]);
 
   return (
-    <CategoryGoodsListContainer>
-      <CategoryFlagContainer>
-        <Link to={`?flag=best&page=1`}>인기순</Link>
-        <Link to={`?flag=latest&page=1`}>신규순</Link>
-        <Link to={`?flag=low&page=1`}>낮은 가격순</Link>
-        <Link to={`?flag=high&page=1`}>높은 가격순</Link>
-      </CategoryFlagContainer>
-      <GoodsSection goodsList={goodsList} itemBoxSize='middle' />
-      <Paginator totalPage={10} currentPage={2} locationSearch={location.search} />
-    </CategoryGoodsListContainer>
+    // TODO: Empty UI 필요
+    goodsListMap && (
+      <CategoryGoodsListContainer>
+        <CategoryGoodsListHeader>
+          <CategoryGoodsListCount>총 {goodsListMap.meta.totalCount}개</CategoryGoodsListCount>
+          <CategoryFlagContainer>
+            <CategoryFlag onClick={() => onChangeFlag(GoodsFlag.best)}>인기순</CategoryFlag>
+            <CategoryFlag onClick={() => onChangeFlag(GoodsFlag.latest)}>신규순</CategoryFlag>
+            <CategoryFlag onClick={() => onChangeFlag(GoodsFlag.low)}>낮은 가격순</CategoryFlag>
+            <CategoryFlag onClick={() => onChangeFlag(GoodsFlag.high)}>높은 가격순</CategoryFlag>
+          </CategoryFlagContainer>
+        </CategoryGoodsListHeader>
+        <GoodsSection goodsList={goodsListMap.goodsList} itemBoxSize='middle' />
+        <Paginator totalPage={10} currentPage={2} />
+      </CategoryGoodsListContainer>
+    )
   );
 };
 
@@ -41,10 +76,36 @@ const CategoryGoodsListContainer = styled.div`
   width: 100%;
 `;
 
-const CategoryFlagContainer = styled.div`
+const CategoryGoodsListHeader = styled.div`
   display: flex;
   width: 100%;
+  justify-content: space-between;
+  font-size: 16px;
+`;
+
+const CategoryGoodsListCount = styled.div`
+  width: 20%;
+`;
+
+const CategoryFlagContainer = styled.div`
+  display: flex;
+  width: 80%;
   justify-content: flex-end;
+  column-gap: 1.25rem;
+`;
+
+const CategoryFlag = styled.button`
+  opacity: 0.8;
+  transition: opacity 0.15s linear;
+  border: 0;
+  background-color: transparent;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 600;
+  font-family: 'Do Hyeon', sans-serif;
+  :hover {
+    opacity: 1;
+  }
 `;
 
 export default CategoryGoodsList;
