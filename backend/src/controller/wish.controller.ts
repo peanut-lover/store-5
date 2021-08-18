3540e030f69008c81fb236f98a3c844b154704d1
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-18 15:39:53 +0900

    update: 찜하기 라우터 미들웨어 추가

--- backend/src/controller/wish.controller.ts
+++ backend/src/controller/wish.controller.ts
@@ -3,14 +3,14 @@ import WishService from '../service/wish.service';
 
 async function createWish(req: Request, res: Response) {
   const userId = req.userId;
-  const goodsId = req.body.goodsId;
+  const goodsId = Number(req.params.id);
   const result = await WishService.createWish(userId, goodsId);
   res.status(201).json({ result });
 }
 
 async function deleteWish(req: Request, res: Response) {
   const userId = req.userId;
-  const goodsId = Number(req.params.goodsId);
+  const goodsId = Number(req.params.id);
   await WishService.deleteWish(userId, goodsId);
   res.sendStatus(204);
 }
