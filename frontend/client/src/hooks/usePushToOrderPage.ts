3eace29727a12b80f485571a450543c5c68ce0d6
eojin-dev <eojin-dev@naver.com>
2021-08-18 17:17:09 +0900

    update : usePushToOrderPage 훅 개선

--- frontend/client/src/hooks/usePushToOrderPage.ts
+++ frontend/client/src/hooks/usePushToOrderPage.ts
@@ -1,26 +1,21 @@
+import { useRecoilState } from 'recoil';
+
 import { usePushHistory } from '@src/lib/CustomRouter';
 import { orderState } from '@src/recoil/orderState';
 import { GoodsBeforeOrder } from '@src/types/Goods';
-import { useEffect, useRef } from 'react';
-import { useRecoilState } from 'recoil';
 
 const usePushToOrderPage = () => {
-  const [orderGoodsList, setOrderState] = useRecoilState(orderState);
+  const [_, setOrderState] = useRecoilState(orderState);
   const pushHistory = usePushHistory();
-  const isInitialEffect = useRef(true);
 
-  const pushToOrderPage = (orderGoodsList: GoodsBeforeOrder[]) => {
+  async function setOrderGoodsList(orderGoodsList: GoodsBeforeOrder[]) {
     setOrderState(orderGoodsList);
-  };
+  }
 
-  useEffect(() => {
-    if (isInitialEffect.current) {
-      isInitialEffect.current = false;
-      return;
-    }
-
+  const pushToOrderPage = async (orderGoodsList: GoodsBeforeOrder[]) => {
+    await setOrderGoodsList(orderGoodsList);
     pushHistory('/order');
-  }, [orderGoodsList]);
+  };
 
   return pushToOrderPage;
 };
