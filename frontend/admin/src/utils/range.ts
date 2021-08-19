eeae0811acb80aefc6802d05a6fdcc4a27f05cb2
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-19 22:26:35 +0900

    update: 클라이언트 디바운스 및 페이지네이터 복사
    - 클라이언트에서 사용되고 있는 디바운스, 페이지네이터를 가져왔습니다!

---
+++ frontend/admin/src/utils/range.ts
@@ -0,0 +1,31 @@
+/**
+ *
+ * @param start range 시작 인덱스
+ * @param end range 끝 인덱스
+ * @param step range step
+ * @returns
+ */
+export const range = function (start: number, end: number, step: number = 1) {
+  let arr = [];
+  let len = 0;
+
+  step = step === undefined ? 1 : step;
+
+  if (arguments.length === 1) {
+    len = start;
+    start = 0;
+    end = start;
+  } else {
+    start = start === undefined ? 1 : start;
+    end = end === undefined ? 1 : end;
+    len = end - start;
+  }
+
+  let i = 0;
+  while (i <= len) {
+    arr.push(start + i * step);
+    i += 1;
+  }
+
+  return arr;
+};
