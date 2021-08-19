2246b44e8e95aca09ccb9ea19258acb243754e8e
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-19 22:27:33 +0900

    update: 상품관리 페이지 스타일링 및 업로드 모달 연동

--- frontend/admin/src/pages/GoodsAdmin/GoodsAdmin.tsx
+++ frontend/admin/src/pages/GoodsAdmin/GoodsAdmin.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from '@src/lib/CustomStyledComponent';
 import GoodsTable from './GoodsTable/GoodsTable';
+import GoodsUploadModal from '@src/portal/GoodsUploadModal/GoodsUploadModal';
 
 const GoodsAdmin = () => {
+  const [openUploadModal, setOpenUploadModal] = useState(false);
   return (
     <GoodsAdminContainer>
-      <Title>상품 관리</Title>
+      <GoodsAdminHeader>
+        <Title>상품 관리</Title>
+        <AddButton
+          onClick={() => {
+            setOpenUploadModal(true);
+          }}
+        >
+          상품 등록
+        </AddButton>
+      </GoodsAdminHeader>
       <GoodsTable />
+      {openUploadModal && <GoodsUploadModal onClose={() => setOpenUploadModal(false)} />}
     </GoodsAdminContainer>
   );
 };
+
+const GoodsAdminContainer = styled('div')`
+  width: 100%;
+  position: relative;
+  margin: 5rem;
+`;
+
+const GoodsAdminHeader = styled('div')`
+  display: flex;
+  align-items: center;
+  column-gap: 3rem;
+  margin-bottom: 20px;
+`;
 
 const Title = styled('h2')`
   font-size: 24px;
   font-weight: 600;
 `;
 
-const GoodsAdminContainer = styled('div')`
-  width: 100%;
-  padding: 5rem;
+const AddButton = styled('button')`
+  cursor: pointer;
+  border: 1px solid #aaa;
+  border-radius: 4px;
+  color: #000;
+  background-color: #fff;
+  margin: 0;
+  padding: 0.5rem 1rem;
+  font-size: 1.125rem;
+  font-weight: bolder;
 `;
 
 export default GoodsAdmin;
