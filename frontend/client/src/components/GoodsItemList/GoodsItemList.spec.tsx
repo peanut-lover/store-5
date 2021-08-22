f45c1cf23ae1a92f78ce2279e4af902944a6f7ba
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-23 01:47:33 +0900

    update: Goods 컴포넌트 테스트 수정
    - 에러처리로 인해 로그인 여부를 확인하게 변경했습니다.
    - 변경한 이후 recoilState 사용으로 인해 테스트에 RecoilRoot를 추가하였습니다.

--- frontend/client/src/components/GoodsItemList/GoodsItemList.spec.tsx
+++ frontend/client/src/components/GoodsItemList/GoodsItemList.spec.tsx
@@ -4,6 +4,7 @@ import { render, cleanup } from '@testing-library/react';
 
 import ProductItemList from './GoodsItemList';
 import { Goods } from 'src/types/Goods';
+import { RecoilRoot } from 'recoil';
 
 describe('ProductItemList Component', () => {
   it('should render all goods items', () => {
@@ -13,7 +14,11 @@ describe('ProductItemList Component', () => {
       { id: 3, thumbnailUrl: 'test_url', title: '맥쥬짠3', price: 30000, discountRate: 0 },
       { id: 4, thumbnailUrl: 'test_url', title: '맥쥬짠4', price: 40000, discountRate: 0 },
     ];
-    const wrapper = render(<ProductItemList goodsList={mock_goods} />);
+    const wrapper = render(
+      <RecoilRoot>
+        <ProductItemList goodsList={mock_goods} />
+      </RecoilRoot>
+    );
     expect(wrapper.getByText('맥쥬짠1')).toBeInTheDocument();
     expect(wrapper.getByText('맥쥬짠2')).toBeInTheDocument();
     expect(wrapper.getByText('맥쥬짠3')).toBeInTheDocument();
