6a2cc5017d5e7c81e900c348587c13ae5b3d7b5e
Kwongiseok <ri2377@naver.com>
2021-08-22 10:05:13 +0900

    update: check-primitive-type 수정

--- backend/src/utils/check.primitive.type.ts
+++ backend/src/utils/check.primitive.type.ts
@@ -4,13 +4,13 @@ export const isNumber = (target: any): boolean => {
 };
 
 export const isString = (target: any): boolean => {
-  if (!(target instanceof String) || typeof target !== 'string') return false;
-  return true;
+  if (typeof target === 'string' || target instanceof String) return true;
+  return false;
 };
 
 // TODO: boolean 0, 1도 판단의 기준으로 삼아야 하는가?
 export const isBoolean = (target: any): boolean => {
-  if (target instanceof Boolean || typeof target === 'boolean') return true;
+  if (typeof target === 'boolean' || target instanceof Boolean) return true;
   if (target === '0' || target === '1' || target === 0 || target === 1) return true;
   return false;
 };
