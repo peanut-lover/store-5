e6d44dee044513515b343544c5e047bd20cbaa39
Kwongiseok <ri2377@naver.com>
2021-08-23 15:26:45 +0900

    add: dashboard에 쓰일 Category 개수 가져오기

--- backend/src/types/response/category.response.ts
+++ backend/src/types/response/category.response.ts
@@ -1,3 +1,10 @@
+type CategoryCount = {
+  name: string;
+  value: number;
+};
+
+export type CategoryCountResponse = CategoryCount[];
+
 export type CategoryResponse = {
   id: number;
   name: string;
