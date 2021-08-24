56e8046aa67af200bd3ce23fe41919a6139b89b5
eojin-dev <eojin-dev@naver.com>
2021-08-24 21:35:00 +0900

    update : ReviewContainer 목업

--- frontend/client/src/components/ReviewContainer/ReviewContainer.tsx
+++ frontend/client/src/components/ReviewContainer/ReviewContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import Paginator from '../Paginator/Paginator';
 import ReviewContainerHeader from '../ReviewContainerHeader/ReviewContainerHeader';
 import ReviewList from '../ReviewList/ReviewList';
@@ -8,12 +9,14 @@ interface Props {}
 // TODO: NoData, Loading
 const ReviewContainer: React.FC<Props> = ({}) => {
   return (
-    <>
+    <Wrapper>
       <ReviewContainerHeader />
       <ReviewList />
       <Paginator totalPage={3} currentPage={1} setPage={() => {}} />
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div``;
 
 export default ReviewContainer;
