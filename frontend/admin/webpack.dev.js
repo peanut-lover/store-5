a37e2144c9fea7fcaedfedd1d92ec9d41cfbc504
peanut-lover <jihoson94@gmail.com>
2021-08-24 18:45:40 +0900

    fix: admin webpack 수정

--- frontend/admin/webpack.dev.js
+++ frontend/admin/webpack.dev.js
@@ -5,11 +5,15 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+
   devServer: {
+    publicPath: '/',
     overlay: true,
     port: 8082,
     stats: 'errors-only',
-    historyApiFallback: true,
+    historyApiFallback: {
+      index: '/dist/admin_index.html',
+    },
     proxy: {
       '/api': 'http://localhost:8080',
     },
