09b783da2052608d8df95d879b9a2f2c7bb3883f
eojin-dev <eojin-dev@naver.com>
2021-08-25 13:51:54 +0900

    update : ReviewImageModal > Dim

--- frontend/client/src/components/ReviewImageModal/ReviewImageModal.tsx
+++ frontend/client/src/components/ReviewImageModal/ReviewImageModal.tsx
@@ -1,7 +1,7 @@
 import Portal from '@src/portal/portal';
 import { Review } from '@src/types/Review';
 import React from 'react';
-import styled from 'styled-components';
+import Dim from '../Dim/Dim';
 
 interface Props {
   initialReview: Review;
@@ -16,15 +16,5 @@ const ReviewImageModal: React.FC<Props> = ({ initialReview, initialIndex, onClos
     </Portal>
   );
 };
-
-const Dim = styled.div`
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  z-index: 1000;
-  background-color: #00000020;
-`;
 
 export default ReviewImageModal;
