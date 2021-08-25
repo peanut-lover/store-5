d16b7c2d04a86eda1765342e138e13a06bdfbf62
Kwongiseok <ri2377@naver.com>
2021-08-25 15:17:10 +0900

    add: RollingNumber 컴포넌트 레이아웃

---
+++ frontend/client/src/components/RollingNumber/RollingNumber.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+  txt: string | number;
+}
+
+const RollingNumber: React.FC<Props> = ({ txt }) => {
+  return <div>hi</div>;
+};
+
+export default RollingNumber;
