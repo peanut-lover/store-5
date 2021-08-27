1d3304c2bd7831e9e3c3b13a1f4faf68b5dedae7
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-27 18:22:51 +0900

    update: find -> get 변경 (네이밍 일관성!)

--- backend/src/repository/user.repository.ts
+++ backend/src/repository/user.repository.ts
@@ -4,7 +4,7 @@ import { User } from '../entity/User';
 import { DatabaseError } from '../errors/base.error';
 
 // TODO: 적절한 반환할 데이터 고민 후에 타입을 지정, 인자로 입력받는 것도 타입을 지정해서 사용하는 것으로
-async function findByGitHubId(githubId: string): Promise<User | undefined> {
+async function getByGitHubId(githubId: string): Promise<User | undefined> {
   try {
     const userRepo = getRepository(User);
     const data = await userRepo.findOne({
@@ -17,7 +17,7 @@ async function findByGitHubId(githubId: string): Promise<User | undefined> {
   }
 }
 
-async function findById(id: number): Promise<User | undefined> {
+async function getById(id: number): Promise<User | undefined> {
   try {
     const userRepo = getRepository(User);
     const data = await userRepo.findOne({
@@ -42,7 +42,7 @@ async function create(githubId: string, name: string, profileImgUrl?: string): P
 }
 
 export const UserRepository = {
-  findByGitHubId,
-  findById,
+  getByGitHubId,
+  getById,
   create,
 };
