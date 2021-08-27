6ed7d864adf1ab733bf87bb271537386cc32bb51
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-27 09:59:45 +0900

    update: 불필요한 코드 제거
    - 사용하지 않는 변수나 파일, 주석 중 제가 작성한 부분만 삭제했습니다!
    
    - index.ts의 hi도 보내줬습니다... ㅠㅠ

--- backend/src/service/goods.service.ts
+++ backend/src/service/goods.service.ts
@@ -28,16 +28,6 @@ export const GoodsStateMap: {
   destroy: 'D',
 };
 
-// 'best' = 인기, 'low' = 낮은 가격, 'high' = 높은 가격, 'latest' = 최신순
-const GoodsFlag: {
-  [keyword: string]: keyof Goods;
-} = {
-  best: 'countOfSell',
-  low: 'price',
-  high: 'price',
-  latest: 'createdAt',
-};
-
 const DEFAULT_START_PAGE = 1;
 
 const INVALID_DISCOUNT_RATE = '할인율은 0~99% 범위 내에서 가능합니다.';
@@ -259,7 +249,6 @@ async function getGoodsByOption(
   };
 }
 
-// TODO: 각 조회를 하나의 함수로 분리?
 async function getMainGoodsListMap(userId?: number): Promise<{
   bestGoodsList: TaggedGoodsType[];
   latestGoodsList: TaggedGoodsType[];
