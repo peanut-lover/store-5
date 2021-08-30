cfd7c293293ce8bdec4def2226934ba6e210be5b
peanut-lover <jihoson94@gmail.com>
2021-08-30 12:28:58 +0900

    add: react-helmet 적용

--- frontend/client/src/index.tsx
+++ frontend/client/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
-import { RecoilRoot } from 'recoil';
 import theme from './theme/theme';
 import App from './App';
+import Helmet from 'react-helmet';
 import ToastProvider from './lib/ToastProvider/ToastProvider';
+import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
 
 const rootElement = document.getElementById('root');
 
@@ -12,6 +13,9 @@ ReactDOM.render(
   <RecoilRoot>
     <ThemeProvider theme={theme}>
       <ToastProvider>
+        <Helmet>
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap'></link>
+        </Helmet>
         <App />
       </ToastProvider>
     </ThemeProvider>
